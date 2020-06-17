3d1f11805a92c4487fef4574b31255063e6aede2
jnolan97 <johnmnolan97@gmail.com>
2020-06-16 19:43:42 -0500

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FunfactsComponent } from './funfacts/funfacts.component';
+import { promises } from 'fs';
+import { PromisesComponent } from './promises/promises.component';
+import { CallbacksComponent } from './callbacks/callbacks.component';
+import { MapComponent } from './map/map.component';
+import { CreatorComponent } from './creator/creator.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'funfacts', component: FunfactsComponent
+  },
+  {
+    path: 'promises', component: PromisesComponent
+  },
+  {
+    path: 'callbacks', component: CallbacksComponent
+  },
+  {
+    path: 'map', component: MapComponent
+  },
+  {
+    path: 'creator', component: CreatorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FunfactsComponent } from './funfacts/funfacts.component';
+import { CallbacksComponent } from './callbacks/callbacks.component';
+import { PromisesComponent } from './promises/promises.component';
+import { MapComponent } from './map/map.component';
+import { CreatorComponent } from './creator/creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FunfactsComponent,
+    CallbacksComponent,
+    PromisesComponent,
+    MapComponent,
+    CreatorComponent
   ],
   imports: [
     BrowserModule,
